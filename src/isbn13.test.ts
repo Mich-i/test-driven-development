34553f42b8aec2da4d4629a93981b761f402c5f6
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 20:43:03 +0200

    Aufgabe 3, weitere Test geschrieben zur Sicherstellung + Code angeschaut, sollte passen

--- src/isbn13.test.ts
+++ src/isbn13.test.ts
@@ -1,10 +1,23 @@
 import { isValid } from './isbn13';
 
 describe('isValid - ISBN validation', () => {
-    test('valid ISBN returns true', () => {
+    test('valid ISBN-13 returns true', () => {
         expect(isValid('9780306406157')).toBe(true);
-    });
-    test('invalid ISBN returns false', () => {
+      });
+    
+      test('invalid ISBN-13 returns false', () => {
         expect(isValid('9780306406158')).toBe(false);
-    });
+      });
+    
+      test('ISBN with letters returns false', () => {
+        expect(isValid('97803A6406157')).toBe(false);
+      });
+    
+      test('ISBN that is too short returns false', () => {
+        expect(isValid('978030640615')).toBe(false);
+      });
+    
+      test('ISBN that is too long returns false', () => {
+        expect(isValid('97803064061577')).toBe(false);
+      });
 });
