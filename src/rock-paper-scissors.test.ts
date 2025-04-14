7b19ef75fc7e5688d0ec1ab81824eb7a04ea59d9
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 20:10:25 +0200

    Aufgabe 2, Test geschrieben damit bei Rock und Rock mal nichts passiert, return 0 + Code Implementation

--- src/rock-paper-scissors.test.ts
+++ src/rock-paper-scissors.test.ts
@@ -7,3 +7,7 @@ test('rock beats scissors', () => {
 test('paper beats rock', () => {
     expect(play('rock', 'paper')).toBe(2);
 })
+
+test('rock and rock is a draw', () => {
+    expect(play('rock', 'rock')).toBe(0);
+})
