246d3bdbe59947da9e1be3d09fef445408e11b73
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 19:47:39 +0200

    Aufgabe 1, Test für formatiereung für Minuten und Sekunden geschrieben + Code implementation

--- src/duration.test.ts
+++ src/duration.test.ts
@@ -3,3 +3,7 @@ import { formatDuration } from './duration';
 test('formats seconds', () => {
     expect(formatDuration(33)).toBe('33s');
   });
+  
+  test('formats minutes and seconds', () => {
+    expect(formatDuration(123)).toBe('2m3s');
+  });
--- src/duration.ts
+++ src/duration.ts
@@ -1,3 +1,11 @@
 export function formatDuration(seconds: number): string {
-  return `${Math.round(seconds)}s`;
+  const totalSeconds = Math.round(seconds);
+  const minutes = Math.floor(totalSeconds / 60);
+  const secs = totalSeconds % 60;
+
+  if (minutes > 0) {
+    return `${minutes}m${secs}s`;
+  } else {
+    return `${secs}s`;
+  }
 }
