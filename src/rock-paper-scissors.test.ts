20cefacc290476bc49cd6e7c9637f0fd64a671f8
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 20:09:02 +0200

    Aufgabe 2, Test geschrieben für fall das Stein Papier schlagt + Code implementation

--- src/rock-paper-scissors.test.ts
+++ src/rock-paper-scissors.test.ts
@@ -3,3 +3,7 @@ import { play } from './rock-paper-scissors';
 test('rock beats scissors', () => {
     expect(play('rock', 'scissors')).toBe(1);
 })
+
+test('paper beats rock', () => {
+    expect(play('rock', 'paper')).toBe(2);
+})
--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,3 +1,14 @@
-export function play(player1: string, player2: string): number {
-  return 1;
+export function play(p1: string, p2: string): number {
+  p1 = p1.trim().toLowerCase();
+  p2 = p2.trim().toLowerCase();
+
+  if (p1 === 'rock' && p2 === 'scissors') {
+    return 1;
+  }
+
+  if (p1 === 'rock' && p2 === 'paper') {
+    return 2;
+  }
+
+  return 0;
 }
