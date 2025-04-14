e77e60574b9b926a9675f85099bf227648142ae6
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 20:39:55 +0200

    Aufgabe 3, Test geschrieben im falle von ungültigem ISBN + Code implementiert/refactored

--- src/isbn13.test.ts
+++ src/isbn13.test.ts
@@ -4,4 +4,7 @@ describe('isValid - ISBN validation', () => {
     test('valid ISBN returns true', () => {
         expect(isValid('9780306406157')).toBe(true);
     });
+    test('invalid ISBN returns false', () => {
+        expect(isValid('9780306406158')).toBe(false);
+    });
 });
--- src/isbn13.ts
+++ src/isbn13.ts
@@ -1,3 +1,16 @@
 export function isValid(isbn: string): boolean {
-  return true;
+  if (isbn.length !== 13 || !/^\d+$/.test(isbn)) { // habe regex benutzt für zu sehen wenn es nicht nur aus Ziffern wöre
+    return false
+  }
+
+  const digits = isbn.split('').map(Number); 
+
+  let sum = 0;
+  for (let i = 0; i < 12; i++) {
+    sum += (i % 2 === 0) ? digits[i] : digits[i] * 3;
+  }
+
+  const checksum = (10 - (sum % 10)) % 10;
+
+  return checksum === digits[12];
 }
