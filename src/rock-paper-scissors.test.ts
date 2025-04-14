9f24278657e5d8c60224ef2ff597c96317043f3e
Michael Stutz <michael_stutz@sluz.ch>
2025-04-14 20:22:42 +0200

    Aufgabe 2, Test geschrieben Papier sollte gegen Schäre verlieren, brauchte keine Code implementation + alle Tests mit einem Describe in eine Testsuite parametisiert, habs vorhin vergessen

--- src/rock-paper-scissors.test.ts
+++ src/rock-paper-scissors.test.ts
@@ -1,17 +1,19 @@
 import { play } from './rock-paper-scissors';
 
-test('rock beats scissors', () => {
-    expect(play('rock', 'scissors')).toBe(1);
-})
-
-test('paper beats rock', () => {
-    expect(play('rock', 'paper')).toBe(2);
-})
-
-test('rock and rock is a draw', () => {
-    expect(play('rock', 'rock')).toBe(0);
-})
-
-test('scissors beats paper', () => {
-    expect(play('scissors', 'paper')).toBe(1);
-})
+describe('play - Rock Paper Scissors', () => {
+    test('rock beats scissors', () => {
+        expect(play('rock', 'scissors')).toBe(1);
+    });
+    test('paper beats rock', () => {
+        expect(play('rock', 'paper')).toBe(2);
+    });
+    test('rock and rock is a draw', () => {
+        expect(play('rock', 'rock')).toBe(0);
+    });
+    test('scissors beats paper', () => {
+        expect(play('scissors', 'paper')).toBe(1);
+    });
+    test('paper loses to scissors', () => {
+        expect(play('paper', 'scissors')).toBe(2);
+    });
+});
